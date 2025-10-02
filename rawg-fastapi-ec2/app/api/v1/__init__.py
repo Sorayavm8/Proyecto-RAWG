2c8481c333657d2e18f9b213870cb1aeb333e670
Sorayavm8 <vmsoraya@gmail.com>
2025-10-02 12:28:55 +0200

    Eliminar rawg-fastapi-ec2 del repositorio y añadir a .gitignore

--- rawg-fastapi-ec2/app/api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from .routes import predict, qa
-
-router = APIRouter()
-router.include_router(predict.router, prefix="/ml", tags=["ml"])
-router.include_router(qa.router, prefix="/qa", tags=["qa"])
